04a4c6cc5d5fd4b0623bd31898e27f7c9a449936
PsychoPervs <carmenmontiel@Carmens-iMac.local>
2025-02-07 15:35:26 -0500

    Use GSAP

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 const jsConfetti = new JSConfetti();
 jsConfetti.addConfetti();
+gsap.from("#text", {
+  duration: 2,           // Duration of the animation (in seconds)
+  text: {                // The TextPlugin will animate the text
+    value: "",           // Start with an empty string
+    delimiter: "",       // The delimiter (no space, no break)
+  },
+  delay: 0.5,            // Optional delay before the animation starts
+});
